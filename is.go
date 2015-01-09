b18073e96b068923b54caa84d85f98caf871c896
Mat Ryer <thatmatryer@gmail.com>
2015-01-08 18:57:30 -0800

    fixes #2

--- is.go
+++ is.go
@@ -2,6 +2,7 @@ package west
 
 import (
 	"reflect"
+	"strings"
 
 	"fmt"
 )
@@ -9,8 +10,12 @@ import (
 // An A type assertions that can be made against
 // a response.
 type A struct {
+	// S is the expected status code.
 	S int
+	// B is the expected body object.
 	B interface{}
+	// H is a map of expected (response) headers from the server.
+	H map[string]string
 }
 
 // Is checks the Respons with the specified A.
@@ -23,12 +28,24 @@ func (r *Response) Is(a A) error {
 // Returns nil if everything is OK.
 func (a A) Is(r *Response) error {
 
+	// check status code
 	if a.S > 0 {
 		if a.S != r.StatusCode {
 			return fmt.Errorf("status code %d != %d", r.StatusCode, a.S)
 		}
 	}
 
+	// check headers
+	if a.H != nil {
+		for k, v := range a.H {
+			actual := r.Header.Get(k)
+			if !strings.Contains(actual, v) {
+				return fmt.Errorf("header '%s' doesn't contain '%s': %s", k, v, actual)
+			}
+		}
+	}
+
+	// check body
 	if a.B != nil {
 
 		switch body := a.B.(type) {
